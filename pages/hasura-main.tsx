72adcd69c0b7def0422d408634b373d2d8528886
namaba <one.2426@gmail.com>
2021-10-11 22:15:34 +0900

    apolloclient

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from '@apollo/client'
+import Link from 'next/link'
+import React, { VFC } from 'react'
+import { Layout } from '../components/Layout'
+import { GET_USERS } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+
+const FetchMain: VFC = () => {
+  const { data, error, loading } = useQuery<GetUsersQuery>(GET_USERS)
+  if (error)
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p className="mb-6 font-bold">Hasura main page</p>
+        console.log(data)
+        {data?.users.map((user) => {
+          return (
+            <p className="my-1" key={user.id}>
+              {user.name}
+            </p>
+          )
+        })}
+        <Link href="/hasura-sub">
+          <a className="mt-6">Next</a>
+        </Link>
+      </Layout>
+    )
+}
+export default FetchMain
